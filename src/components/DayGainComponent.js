672b0519fe48fca2190f4594904435a6adfc7bc3
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-09-25 23:44:58 -0400

    Dashbord Statistic Updates

--- src/components/DayGainComponent.js
+++ src/components/DayGainComponent.js
@@ -5,25 +5,8 @@ class DayGain extends React.Component {
   render() {
     return (
       <div>
-        <Statistic
-          value={13073.12}
-          valueStyle={{
-            color: "#0FBE00",
-            fontFamily: "Calistoga",
-            fontSize: "52px",
-          }}
-          prefix="$"
-        />
-        <Statistic
-          value={7.2}
-          valueStyle={{
-            color: "#0FBE00",
-            fontFamily: "Calistoga",
-            fontSize: "52px",
-          }}
-          prefix="("
-          suffix="%)"
-        />
+        <h2>$13,073.12</h2>
+        <h2>(+7.2%)</h2>
       </div>
     );
   }
