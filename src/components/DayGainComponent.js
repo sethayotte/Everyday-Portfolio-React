4bf537a66fe0e3fa9d8dc96a592f3305bb8d7c5e
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-10-16 20:30:48 -0400

    Day Gain Update

--- src/components/DayGainComponent.js
+++ src/components/DayGainComponent.js
@@ -4,7 +4,7 @@ class DayGain extends React.Component {
   render() {
     return (
       <div>
-        <h2>$13,073.14</h2>
+        <h2>$13,073.12</h2>
         <h2>(+7.2%)</h2>
       </div>
     );
